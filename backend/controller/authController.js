23d684c0086e3e9270f19b832f72825c49662ed4
prathamrasal <prathamrasal6@gmail.com>
2022-08-09 15:18:27 +0530

    Merged User Controller To Auth  Controller

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -4,6 +4,97 @@ const catchAsync = require('../utils/catchAsync')
 const { sendEmail } = require('../utils/sendEmail')
 const createToken = require('../utils/createToken')
 const bcrypt = require('bcrypt');
+const aishe = require('../utils/aishe.json')
+
+exports.loginUser = catchAsync(async (req, res, next) => {
+    let success = false;
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return next(
+            new AppError('User Not Exists', 404)
+        )
+    }
+    const passCompare = await bcrypt.compare(password, user.password)
+    if (!passCompare) {
+        return next(
+            new AppError('Try Logging In with Correct Credentials', 404)
+        )
+    }
+    success = true;
+    const authToken = createToken(user.id)
+    res.cookie('auth', authToken)
+    res.status(200).json({ success, authToken });
+})
+
+exports.createUser = catchAsync(async (req, res, next) => {
+    const { email, password, aisheCode } = req.body;
+    const checkMail = await User.find({ email })
+    if (checkMail.length != 0) {
+        return next(
+            new AppError('Email Already Exists', 400)
+        )
+    }
+    if (!(email && password)) {
+        return next(
+            new AppError('Please Provide Email and Password')
+        )
+    }
+    let isAishe = false
+    let instituteName = ""
+    for (let i = 0; i < aishe.length; i++) {
+        if (aishe[i].code === aisheCode) {
+            isAishe = true,
+            instituteName = aishe[i].instituteName
+        }
+    }
+    if (!isAishe) return next(new AppError('Invalid Aishe Code Entered', 403))
+    const salt = await bcrypt.genSalt();
+    const hashPass = await bcrypt.hash(password, salt);
+    const newUser = new User({
+        instituteName,
+        email,
+        aisheCode,
+        password: hashPass
+    })
+    const user = await newUser.save();
+    res.status(201).json({ success: true, message: "Succesfully Created" });
+})
+
+exports.logoutUser = catchAsync((req, res, next) => {
+    res.clearCookie('auth')
+    res.json({ success: true, message: 'Logging Out' })
+})
+
+exports.getUser = catchAsync(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select("-password")
+    if (!user) {
+        return next(
+            new AppError('User Not Found')
+        )
+    }
+    res.json({ success: true, user })
+})
+
+exports.removeUser = catchAsync(async (req, res, next) => {
+    const removedUser = await User.findByIdAndRemove(req.params.id)
+    if (!removedUser) {
+        return next(
+            new AppError('Invalid User Id Provided')
+        )
+    }
+    res.json({ success: true, message: "User removed Succesfully", removedUser })
+})
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    if (!updatedUser) {
+        return next(
+            new AppError('Invalid User Id Provided')
+        )
+    }
+    res.json({ success: true, message: "User Updated Succesfully", updatedUser })
+})
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
     const { email } = req.body;
--- backend/routes/auth-route.js
+++ backend/routes/auth-route.js
@@ -1,32 +1,31 @@
 const router = require('express').Router()
 const checkAuth = require('../middlewares/checkAuth');
-const userController = require('../controller/userController');
 const restrictTo = require('../middlewares/restrictTo');
 const authController = require('../controller/authController')
 
 router
     .route("/login")
-    .post(userController.loginUser)
+    .post(authController.loginUser)
 
 router
     .route("/createUser")
-    .post(userController.createUser)
+    .post(authController.createUser)
 
 router
     .route("/logout")
-    .get(checkAuth, userController.logoutUser)
+    .get(checkAuth, authController.logoutUser)
 
 router
     .route("/updateUser/:id")
-    .patch(checkAuth, userController.updateUser)
+    .patch(checkAuth, authController.updateUser)
 
 router
     .route("/removeUser/:id")
-    .delete(checkAuth, restrictTo('admin'), userController.removeUser)
+    .delete(checkAuth, restrictTo('admin'), authController.removeUser)
 
 router
     .route("/getUser")
-    .get(checkAuth, userController.getUser)
+    .get(checkAuth, authController.getUser)
 
 router
     .route('/forgotPassword')
