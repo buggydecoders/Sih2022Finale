e80854dd349a487ac73de440b828066830baf364
ShivangM <shivangmishra0824@gmail.com>
2022-08-09 14:15:33 +0530

    Auth pages compleated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,19 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Routes>
         <Route exact path="/" element={<Dashboard />}></Route>
         <Route exact path="/profile" element={<Profile />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+        <Route exact path="/signup" element={<Signup />}></Route>
       </Routes>
     </QueryClientProvider>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,26 +1,17 @@
 import React from "react";
-import logo from "../assets/UGC_LOGO.png";
 import { AiOutlineHome, AiFillBell } from "react-icons/ai";
 import {Link} from 'react-router-dom'
 import {BiCheckDouble} from 'react-icons/bi'
 import {GrStatusCritical} from 'react-icons/gr'
 import {BsFillBagCheckFill} from 'react-icons/bs'
 import {RiAccountCircleLine} from 'react-icons/ri'
+import UGCLogo from "./UGCLogo";
 
 function Navbar() {
   return (
     <div className="flex items-center justify-between px-6 py-3">
-      <div className="flex items-center">
-        <Link to="/" className="pr-2">
-            <img src={logo} alt="UGC LOGO" className="w-16 h-16"/>
-        </Link>
-        <div className="flex flex-col">
-          <p className="text-gray-700 font-medium">
-            University Grants Commission
-          </p>
-          <p className="text-gray-500 text-xs">Higher Education Institutes </p>
-        </div>
-      </div>
+      <UGCLogo/>
+
       <ul className="flex font-medium items-center space-x-10">
         <Link to="/" className="text-primary flex items-center space-x-1">
           <AiOutlineHome />
