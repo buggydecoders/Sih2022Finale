b27b47940af6377479aa22d3307011bb1fef6942
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-08-10 01:29:36 +0530

    fixed save items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
+import SavedItems from "./pages/SavedItems";
 import Status from "./pages/Status";
 const queryClient = new QueryClient();
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Route exact path="/" element={<Dashboard />}></Route>
         <Route exact path="/profile" element={<Profile />}></Route>
         <Route exact path="/status" element={<Status />}></Route>
+        <Route exact path="/saved-items" element={<SavedItems />}></Route>
       </Routes>
     </QueryClientProvider>
   );
---
+++ frontend/src/pages/SavedItems.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Search from "../components/Status/Search";
+import ResourceImg from "../assets/Resources/3dPrinter.png";
+import Pagination from "../components/Pagination";
+
+const ProductItem = () => {
+  return (
+    <div className="px-3 py-3 border-[1px] border-gray-300 rounded-xl">
+      <div className="">
+        <img src={ResourceImg} className="w-[130px]" alt="" />
+      </div>
+      <div className="font-[500] mt-7">
+        3d Printer (Lightning version with 32 Gb Ram)
+      </div>
+      <div className="text-sm text-gray-500 font-[500] mt-1">IET, DAVV</div>
+      <div className="text-primary font-bold mt-2 text-lg">
+        Rs. 1500{" "}
+        <span className="text-gray-500 font-[400] text-sm ml-2">/ DAY</span>
+      </div>
+      <div className="w-full text-center bg-secondary text-white py-2 rounded-xl mt-3 bg-opacity-95 cursor-pointer">
+        Request Resource
+      </div>
+    </div>
+  );
+};
+
+const SavedItems = () => {
+  return (
+    <Layout>
+      <div className="px-16 py-10">
+        <div className="text-3xl font-[700] mb-6">Status</div>
+        <Search />
+        <div className="grid-cols-5 grid mt-12 gap-10">
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+        </div>
+      </div>
+      <Pagination/>
+    </Layout>
+  );
+};
+
+export default SavedItems;
