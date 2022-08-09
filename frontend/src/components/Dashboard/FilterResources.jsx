33124b5d7609b4a20ac82a41751d8883729bcd87
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-08-09 13:33:28 +0530

    added filters

--- frontend/src/components/Dashboard/FilterResources.jsx
+++ frontend/src/components/Dashboard/FilterResources.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import CardCollection from './CardCollection'
+import FilterCheckbox from './FilterCheckbox'
+
+const locations = ['Indore', 'Bhopal', 'Salem'];
+const Universities = ['DAVV', 'TIME', 'SONA'];
+const Budget = ['5000-3000', '4000-6000'];
 
 const FilterResources = () => {
   return (
@@ -8,9 +13,16 @@ const FilterResources = () => {
             <div className='text-gray-600 text-xl font-[600]'>FILTERS</div>
             <div className='text-secondary underline text-sm cursor-pointer'>Clear all</div>
         </div>
-        <CardCollection action='clear all' title={'Location'}/>
-        <CardCollection action='clear all' title={'University'}/>
-        <CardCollection action='clear all' title={'Budget'}/>
+        <CardCollection action='clear all' title={'Location'}>
+          {locations.map(l=><FilterCheckbox title={l}/>)}
+        </CardCollection>
+        <CardCollection action='clear all' title={'University'}>
+        {Universities.map(l=><FilterCheckbox title={l}/>)}
+
+        </CardCollection>
+        <CardCollection action='clear all' title={'Budget'}>
+        {Budget.map(l=><FilterCheckbox title={l}/>)}
+        </CardCollection>
     </div>
   )
 }
