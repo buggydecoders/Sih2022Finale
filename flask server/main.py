b10736ef6002cd48040d76892ae554d094b2726b
prathamrasal <prathamrasal6@gmail.com>
2022-08-15 15:42:37 +0530

    Recommendation System & SA Flask Integration

--- flask server/main.py
+++ flask server/main.py
@@ -1,51 +1,11 @@
-import pandas as pd
-import numpy as np
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import nltk
-# nltk.download('vader_lexicon')
-# nltk.download('wordnet')
-# nltk.download('omw-1.4')
-# nltk.download('punkt')
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.stem import PorterStemmer
-from flask import Flask, request, jsonify
+from api import create_app
+import os
+app = create_app()
 
-sentiments = SentimentIntensityAnalyzer()
-lematizer = WordNetLemmatizer()
-
-keywords = {
-    "bad": -4,
-    "angry": -4,
-    "hot": -4,
-    "hotter": -4,
-    "not up to mark": -4
-}
-
-def getCleanText(text):
-    text = text.lower()
-    words = word_tokenize(text)
-    wordd = []
-    for w in words:
-      word = lematizer.lemmatize(w)
-      wordd.append(word)
-    cleanText = ' '.join(wordd)
-    return cleanText
-
-def getSAScore(sentence):
-    sentence = getCleanText(sentence)
-    sentiments.lexicon.update(keywords)
-    score=sentiments.polarity_scores(sentence)['compound']
-    return score
-
-app = Flask(__name__)
-
-@app.route("/",methods=['post'])
-def home():
-    sent = request.form['feedback']
-    score = getSAScore(sent)
-    return jsonify(score)
+UPLOAD_FOLDER = 'static/files'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
