bafde786c4ab1d95eeadd56a30955bf8aa41fc26
Kunal Sangtiani <kunalsangtiani03@gmail.com>
2022-08-17 15:21:44 +0530

    added banner profile addition

--- frontend/src/components/ProfileV2/BannerAndProfile.jsx
+++ frontend/src/components/ProfileV2/BannerAndProfile.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import UNI_LOGO from '../../assets/DAVV_LOGO.png';
 import { useNavigate } from 'react-router-dom'
 import { data } from 'autoprefixer';
+import {AiFillEdit} from 'react-icons/ai';
+import { BANNER_FALLBACK_IMG } from '../../utils/fallbackImages';
+import { getFileLink } from '../../utils/generateImageLink';
+import { updateUser } from '../../store/auth/actions';
 
 const BannerAndProfile = () => {
   const { user } = useSelector(state => state.auth)
+
+  const [uploadLoading,setUploadLoading] = useState(false);
+  const [banner,setBanner] = useState(user?.banner || BANNER_FALLBACK_IMG)
+  const dispatch = useDispatch();
+
+  const handleChangeBanner = async(e)=>{
+    if (e.target.files.length>0) {
+      let file = e.target.files[0];
+      setUploadLoading(true);
+      const link = await getFileLink(file);
+      const successFallBack = ()=>{
+        setBanner(link);
+        setUploadLoading(false);
+      }
+      const errorFallBack = ()=>{setUploadLoading(false)}
+      dispatch(updateUser({banner : link}, successFallBack,errorFallBack));
+
+    }
+
+  }
+
   const navigate = useNavigate();
   return (
     <div className=''>
-      <div className='w-full h-[30vh] bg-gray-200 rounded-md relative ' style={{ background: `url(${user.banner ? user.banner : 'https://res.cloudinary.com/unesco-admin/image/upload/v1660722255/banner-placeholder_nyqa5j.webp'}) center center/cover` }}>
+      <div className='w-full h-[30vh] bg-gray-200 rounded-md relative ' style={{ background: `url(${banner}) center center/cover` }}>
+        <div className='flex relative flex-row-reverse gap-3 p-3 text-gray-500 cursor-pointer items-center text-lg font-open'>
+          <input
+          type="file"
+          className='absolute opacity-0'
+          onChange={handleChangeBanner}
+          disabled={uploadLoading}
+          />
+          <button className='flex text-black'>
+          <AiFillEdit className='text-black'/>
+          </button>
+          <div className='text-sm'>{uploadLoading?'Loading..':"Recommeded Size : 1400x1600"}</div>
+        </div>
         <div onClick={() => navigate('/edit-profile')} className='border-[1px] border-primary text-primary w-fit -bottom-2 translate-y-[100%] px-5 rounded-xl hover:bg-primary hover:text-white duration-300 transition-all ease-in-out cursor-pointer right-0 absolute'>Edit Profile</div>
       </div>
       <div className='flex justify-between items-center'>
